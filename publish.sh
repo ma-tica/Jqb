c13862e18b16c0e055ad507d962dc3e556ba1887
Matteo Carminati <mcarminati@mcmatica.it>
2018-05-28 10:55:24 +0200

    add gitignore

--- publish.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-mvn install:install-file -DgroupId=org.mcmatica -DartifactId=jqb -Dversion=0.0.8 -Dpackaging=jar -Dfile=/datadisk/Clienti/TEST/workspace/jqb/target/jqb-0.0.8.jar -DlocalRepositoryPath=/datadisk/Clienti/MATICA/maven-repo
